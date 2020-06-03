0abb0ec2d48fed5e71a4f31dc49b844887571e00
nimingShare <niming_share@163.com>
2020-06-03 21:47:41 +0800

    初始化项目提交

---
+++ client/layout/footer.jsx
@@ -0,0 +1,18 @@
+// import className from '../assets/styles/footer.styl'
+import '../assets/styles/footer.styl'
+
+export default {
+  data () {
+    return {
+      author: 'Jokcy'
+    }
+  },
+  render () {
+    return (
+      // <div id={className.footer}>
+      <div id="footer">
+        <span>Written by {this.author}</span>
+      </div>
+    )
+  }
+}
